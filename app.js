b577e84a07f1c326c75b11a913146fc686c86e84
Yogesh Pandey <yogbpl@gmail.com>
2020-05-19 18:31:07 +0530

    new todo list

---
+++ app.js
@@ -0,0 +1,219 @@
+"use strict";
+// autobind decorator
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDesctiptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDesctiptor;
+}
+function formValidation(validatableInput) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid =
+            isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    return isValid;
+}
+// Todo Type
+var TodoStatus;
+(function (TodoStatus) {
+    TodoStatus[TodoStatus["Active"] = 0] = "Active";
+    TodoStatus[TodoStatus["Finished"] = 1] = "Finished";
+})(TodoStatus || (TodoStatus = {}));
+class Todo {
+    constructor(id, title, date, status) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.status = status;
+    }
+}
+class State {
+    constructor() {
+        this.listeners = [];
+    }
+    addListener(listenerFn) {
+        this.listeners.push(listenerFn);
+    }
+}
+// Todo State Management
+class TodoState extends State {
+    constructor() {
+        super();
+        this.todos = [];
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new TodoState();
+        return this.instance;
+    }
+    addTodo(title) {
+        const dateTime = new Date().toDateString();
+        const newTodo = new Todo(Math.random().toString(), title, dateTime, TodoStatus.Active);
+        this.todos.push(newTodo);
+        this.updateListeners();
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+    }
+    moveTodo(todoId, newStatus) {
+        const todo = this.todos.find(tod => tod.id === todoId);
+        if (todo) {
+            todo.status = newStatus;
+            this.updateListeners();
+        }
+    }
+    updateListeners() {
+        for (const listenerFn of this.listeners) {
+            listenerFn(this.todos.slice());
+        }
+    }
+}
+const todoState = TodoState.getInstance();
+// Component Base Class
+class Component {
+    constructor(templateId, hostElementId, insertAtStart, newElementId) {
+        this.tempalteElement = document.getElementById(templateId);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.tempalteElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.render(insertAtStart);
+    }
+    render(insertAtBeginning) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);
+    }
+}
+// TodoItem Class
+class TodoItem extends Component {
+    constructor(hostId, todo) {
+        super('single-todo', hostId, false, todo.id);
+        this.todos = [];
+        this.todo = todo;
+        this.configure();
+        this.renderContent();
+    }
+    setCompleteHandler(event) {
+        const todoId = this.element.id;
+        todoState.moveTodo(todoId, TodoStatus.Finished);
+    }
+    configure() {
+        this.element.querySelector('input').addEventListener('click', this.setCompleteHandler);
+    }
+    renderContent() {
+        this.element.querySelector('h4').textContent = this.todo.title;
+        this.element.querySelector('small').textContent = this.todo.date;
+    }
+}
+__decorate([
+    autobind
+], TodoItem.prototype, "setCompleteHandler", null);
+__decorate([
+    autobind
+], TodoItem.prototype, "configure", null);
+//TodoList class
+class TodoList extends Component {
+    constructor(type) {
+        super('todo-list', 'todo-container', false, `${type}-todos`);
+        this.type = type;
+        this.assignedTodos = [];
+        this.configure();
+        this.renderContent();
+    }
+    configure() {
+        todoState.addListener((todos) => {
+            const relevantTodos = todos.filter(tod => {
+                if (this.type === 'active') {
+                    return tod.status === TodoStatus.Active;
+                }
+                return tod.status === TodoStatus.Finished;
+            });
+            this.assignedTodos = relevantTodos;
+            this.renderTodos();
+        });
+    }
+    ;
+    renderContent() {
+        const listId = `${this.type}-todo-list`;
+        this.element.querySelector('ul').id = listId;
+        if (this.type === 'active') {
+            this.element.querySelector('label').className = 'text-warning';
+            this.element.querySelector('label').innerHTML = `<i class="fa fa-exclamation-triangle"></i> ${this.type} todos`;
+        }
+        else {
+            this.element.querySelector('label').className = 'text-success';
+            this.element.querySelector('label').innerHTML = `<i class="fa fa-check"></i> ${this.type} todos`;
+        }
+    }
+    renderTodos() {
+        const listEl = document.getElementById(`${this.type}-todo-list`);
+        listEl.innerHTML = '';
+        for (const todoItem of this.assignedTodos) {
+            new TodoItem(this.element.querySelector('ul').id, todoItem);
+        }
+    }
+}
+// Todo Class
+class TodoInput extends Component {
+    constructor(template, elem) {
+        super(template, elem, true);
+        this.template = template;
+        this.elem = elem;
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.configure();
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    renderContent() { }
+    gatherUserInput() {
+        const enterDescription = this.descriptionInputElement.value;
+        const descriptionValidatable = {
+            value: enterDescription,
+            required: true,
+        };
+        if (!formValidation(descriptionValidatable)) {
+            swal("invalid input", "Please try again", "error");
+            return;
+        }
+        else
+            return [enterDescription];
+    }
+    clearInput() {
+        this.descriptionInputElement.value = '';
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [desc] = userInput;
+            todoState.addTodo(desc);
+            this.clearInput();
+        }
+    }
+}
+__decorate([
+    autobind
+], TodoInput.prototype, "submitHandler", null);
---
+++ app.ts
@@ -0,0 +1,285 @@
+// autobind decorator
+
+function autobind(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDesctiptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDesctiptor;
+}
+
+// Validation
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+}
+
+function formValidation(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (
+        validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid =
+            isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (
+        validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid =
+            isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    return isValid;
+}
+
+
+// Move Active to Finished Interface
+
+interface SourseTodo {
+    setCompleteHandler(event: MouseEvent): void;
+}
+
+interface TargetTodo {
+    setActiveHandler(event: MouseEvent): void;
+}
+
+
+
+// Todo Type
+enum TodoStatus { Active, Finished }
+
+class Todo {
+
+    constructor(
+        public id: string,
+        public title: string,
+        public date: string,
+        public status: TodoStatus) {
+
+    }
+}
+
+type Listner<T> = (items: T[]) => void;
+
+class State<T> {
+
+    protected listeners: Listner<T>[] = [];
+
+    addListener(listenerFn: Listner<T>) {
+        this.listeners.push(listenerFn);
+    }
+
+}
+// Todo State Management
+class TodoState extends State<Todo> {
+
+    private todos: Todo[] = [];
+    private static instance: TodoState;
+
+    private constructor() {
+        super()
+    }
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new TodoState()
+        return this.instance;
+    }
+
+    addTodo(title: string) {
+        const dateTime: string = new Date().toDateString();
+        const newTodo = new Todo(Math.random().toString(), title, dateTime, TodoStatus.Active);
+        this.todos.push(newTodo);
+        this.updateListeners();
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+    }
+
+    moveTodo(todoId: string, newStatus: TodoStatus) {
+        const todo = this.todos.find(tod => tod.id === todoId);
+        if (todo) {
+            todo.status = newStatus;
+            this.updateListeners()
+        }
+    }
+
+
+    private updateListeners() {
+        for (const listenerFn of this.listeners) {
+            listenerFn(this.todos.slice());
+        }
+    }
+}
+
+const todoState = TodoState.getInstance();
+
+
+// Component Base Class
+
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    tempalteElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string) {
+        this.tempalteElement = document.getElementById(templateId) as HTMLTemplateElement;
+        this.hostElement = document.getElementById(hostElementId) as T;
+        const importedNode = document.importNode(this.tempalteElement.content, true);
+        this.element = importedNode.firstElementChild as U;
+        if (newElementId) {
+            this.element.id = newElementId;
+        }
+        this.render(insertAtStart);
+    }
+    private render(insertAtBeginning: boolean) {
+        this.hostElement!.insertAdjacentElement(
+            insertAtBeginning ? 'afterbegin' : 'beforeend',
+            this.element);
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+
+}
+// TodoItem Class
+class TodoItem extends Component<HTMLUListElement, HTMLLIElement>
+    implements SourseTodo {
+    private todo: Todo;
+    private todos: Todo[] = [];
+
+
+    constructor(hostId: string, todo: Todo) {
+        super('single-todo', hostId, false, todo.id);
+        this.todo = todo;
+        this.configure();
+        this.renderContent();
+    }
+
+    @autobind
+    setCompleteHandler(event: MouseEvent) {
+        const todoId = this.element.id;
+
+        todoState.moveTodo(todoId, TodoStatus.Finished);
+    }
+
+    @autobind
+    configure() {
+        this.element.querySelector('input')!.addEventListener('click', this.setCompleteHandler)
+    }
+
+    renderContent() {
+        this.element.querySelector('h4')!.textContent = this.todo.title;
+        this.element.querySelector('small')!.textContent = this.todo.date;
+    }
+
+}
+
+//TodoList class
+class TodoList extends Component<HTMLDivElement, HTMLElement>
+{
+
+    assignedTodos: Todo[];
+    constructor(private type: 'active' | 'finished') {
+        super('todo-list', 'todo-container', false, `${type}-todos`);
+
+        this.assignedTodos = [];
+
+        this.configure();
+        this.renderContent();
+
+    }
+
+
+    configure() {
+
+        todoState.addListener((todos: Todo[]) => {
+            const relevantTodos = todos.filter(tod => {
+                if (this.type === 'active') {
+                    return tod.status === TodoStatus.Active;
+                }
+                return tod.status === TodoStatus.Finished;
+            })
+
+            this.assignedTodos = relevantTodos;
+            this.renderTodos();
+        });
+    };
+    renderContent() {
+        const listId = `${this.type}-todo-list`;
+        this.element.querySelector('ul')!.id = listId;
+        if (this.type === 'active') {
+            this.element.querySelector('label')!.className = 'text-warning';
+            this.element.querySelector('label')!.innerHTML = `<i class="fa fa-exclamation-triangle"></i> ${this.type} todos`;
+        }
+        else {
+            this.element.querySelector('label')!.className = 'text-success';
+            this.element.querySelector('label')!.innerHTML = `<i class="fa fa-check"></i> ${this.type} todos`;
+        }
+
+    }
+    private renderTodos() {
+        const listEl = document.getElementById(`${this.type}-todo-list`) as HTMLUListElement;
+        listEl.innerHTML = '';
+        for (const todoItem of this.assignedTodos) {
+            new TodoItem(this.element.querySelector('ul')!.id, todoItem);
+        }
+    }
+}
+
+// Todo Class
+class TodoInput extends Component<HTMLDivElement, HTMLFormElement> {
+    descriptionInputElement: HTMLTextAreaElement;
+
+    constructor(public template: string, public elem: string) {
+        super(template, elem, true);
+        this.descriptionInputElement = this.element.querySelector("#description") as HTMLTextAreaElement;
+        this.configure();
+    }
+
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler)
+    }
+
+    renderContent() { }
+
+    private gatherUserInput(): [string] | undefined {
+        const enterDescription = this.descriptionInputElement.value;
+        const descriptionValidatable: Validatable = {
+            value: enterDescription,
+            required: true,
+        };
+        if (!formValidation(descriptionValidatable)) {
+            swal("invalid input", "Please try again", "error");
+            return;
+        }
+        else
+            return [enterDescription];
+    }
+    private clearInput() {
+        this.descriptionInputElement.value = '';
+    }
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput()
+        if (Array.isArray(userInput)) {
+            const [desc] = userInput;
+            todoState.addTodo(desc);
+            this.clearInput();
+        }
+
+    }
+}
